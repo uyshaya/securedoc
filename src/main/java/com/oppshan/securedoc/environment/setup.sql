7d867c1d312944c7decbe36604a0989bb8751608
Shaniah Uy <uyshaya@gmail.com>
2026-05-04 23:24:51 +0800

    Add landing page and db schema; Resolve #2 #3

--- src/main/java/com/oppshan/securedoc/environment/setup.sql
+++ src/main/java/com/oppshan/securedoc/environment/setup.sql
@@ -1,5 +1,8 @@
+CREATE DATABASE IF NOT EXISTS securedoc
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
 -- dedicated user for the app
-CREATE DATABASE IF NOT EXISTS securedoc;
 CREATE USER 'securedoc_user'@'localhost' IDENTIFIED BY 'securedoc!123';
 GRANT ALL PRIVILEGES ON securedoc.* TO 'securedoc_user'@'localhost';
 FLUSH PRIVILEGES;
